3a886bf97b57e57293ca733d0ae516ee77e92d7d
test <likehouse2@naver.com>
2025-04-03 14:01:49 +0900

    확인용

---
+++ hanbin/src/main/java/com/project4/hanbin/HanbinApplication.java
@@ -0,0 +1,13 @@
+package com.project4.hanbin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanbinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanbinApplication.class, args);
+	}
+
+}
---
+++ hanbin/src/test/java/com/project4/hanbin/HanbinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project4.hanbin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanbinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
