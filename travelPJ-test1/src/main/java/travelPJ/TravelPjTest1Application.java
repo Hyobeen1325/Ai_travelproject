718bd53a347fdd7470c2a3d915af6684abe7c186
유찬우 <dbcksdnqkqh@naver.com>
2025-04-04 11:15:02 +0900

    1114 커밋

---
+++ travelPJ-test1/src/main/java/travelPJ/TravelPjTest1Application.java
@@ -0,0 +1,13 @@
+package travelPJ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelPjTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelPjTest1Application.class, args);
+	}
+
+}
