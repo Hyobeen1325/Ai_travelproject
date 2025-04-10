0345414528e2b037b6eb6db8131ae38215af1a63
jeonghyeonkwon148 <wjd90gus@naver.com>
2025-04-10 15:03:33 +0900

    commit

--- final_project/src/main/java/com/example/demo/controller/JHController.java
+++ final_project/src/main/java/com/example/demo/controller/JHController.java
@@ -1,10 +1,5 @@
 package com.example.demo.controller;
 
-import ch.qos.logback.core.recovery.ResilientSyslogOutputStream;
-
 public class JHController {
-	int a = 55;
-	private void sysout() {
-		System.out.println("hi");
-	}
+
 }
