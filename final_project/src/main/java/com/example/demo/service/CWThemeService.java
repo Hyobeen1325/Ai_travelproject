6e8622117ed00d3e3146fd2ae48876994287ba60
유찬우 <dbcksdnqkqh@naver.com>
2025-04-18 11:36:01 +0900

    선택값 CRUD 서비스단 예외처리

--- final_project/src/main/java/com/example/demo/service/CWThemeService.java
+++ final_project/src/main/java/com/example/demo/service/CWThemeService.java
@@ -25,9 +25,12 @@ public class CWThemeService {
 
     // 선택값 등록
     public String insertChooseVal(CWThemeCRUDRequest themeRequest) {
-    	CWThemeResponse response = restTemplate.postForObject(fastApiUrl + "/choose_val", themeRequest, CWThemeResponse.class);
-    	
-    	return response!=null?"등록 성공":"등록 실패";
+    	try {
+    		restTemplate.postForObject(fastApiUrl + "/choose_val", themeRequest, CWThemeResponse.class);
+    		return "등록성공";
+		} catch (Exception e) {
+			return "등록실패\n 사유: "+e.getMessage();
+		}
     }
 
     // 선택값 전체 조회
@@ -38,24 +41,43 @@ public class CWThemeService {
 
     // 선택값 단일조회
     public CWThemeResponse getChooseValById(int choose_id) {
-        return restTemplate.getForObject(fastApiUrl + "/choose_val/" + choose_id, CWThemeResponse.class);
+    	try {
+    		return restTemplate.getForObject(fastApiUrl + "/choose_val/" + choose_id, CWThemeResponse.class);
+		} catch (HttpClientErrorException e) {
+			CWThemeResponse noFind = new CWThemeResponse();
+			noFind.setHigh_loc("해당 ID의 선택값은 없음");
+			return noFind;
+		} catch (Exception e) {
+			CWThemeResponse faile = new CWThemeResponse();
+			faile.setHigh_loc("탐색실패");
+			faile.setLow_loc(e.getMessage());
+			return faile;
+		}
     }
 
     // 선택값 수정
     public String updateChooseVal(int choose_id, CWThemeCRUDRequest themeRequest) {
-    	restTemplate.put(fastApiUrl + "/choose_val/" + choose_id, themeRequest);
-    	CWThemeResponse response = getChooseValById(choose_id);
-    	return response!=null?"수정 성공":"수정 실패";
+    	try {
+    		restTemplate.put(fastApiUrl + "/choose_val/" + choose_id, themeRequest);
+    		return "수정성공";
+		} catch (Exception e) {
+			return "수정실패\n 사유: "+e.getMessage();
+		}
     }
 
     // 선택값 삭제
     public String deleteChooseVal(int choose_id) {
-    	restTemplate.delete(fastApiUrl + "/choose_val/" + choose_id);
+
     	try {
-    		CWThemeResponse choose_val = getChooseValById(choose_id);
-    		return "삭제실패\n 삭제정보 조회됨";
-		} catch (HttpClientErrorException e) {
-			return "삭제 성공\n삭제된 선택값 ID: " + choose_id;
+	    	restTemplate.delete(fastApiUrl + "/choose_val/" + choose_id);
+	    	try {
+	    		getChooseValById(choose_id);
+	    		return "삭제실패\n 삭제정보 조회됨";
+			} catch (HttpClientErrorException e) {
+				return "삭제 성공\n삭제된 선택값 ID: " + choose_id;
+			} catch (Exception e) {
+				return "삭제실패\n 사유: "+e.getMessage();
+			}
 		} catch (Exception e) {
 			return "삭제실패\n 사유: "+e.getMessage();
 		}
