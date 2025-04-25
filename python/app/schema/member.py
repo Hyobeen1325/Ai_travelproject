1b5e960c19aa198a48f02d71ce17dae0b3743dc8
seoyoung <wsxy70866@gmail.com>
2025-04-25 11:42:59 +0900

    세션 에러 해결(config 추가)

--- python/app/schema/member.py
+++ python/app/schema/member.py
@@ -11,30 +11,36 @@ class MemberBase(BaseModel):  # member 테이블 베이스 모델
     phon_num: str  # 전화번호
     reg_date: datetime # 생성일 (기본값 : 현재 시각)
     upt_date: datetime  # 수정일 (기본값 : 현재 시각)
-    
-    class Config: # 일부 회원정보 조회
-        orm_mode = True # orm model 처리 
+    class Config: # 일부 유저 조회
+        orm_mode = True
 
 class LoginModel(BaseModel): # 로그인 모델
     email: str
     pwd: str
-
+    class Config: 
+        orm_mode = True
     
 class MypageModel(BaseModel): # 마이페이지 조회 모델
     email: str # 이메일(아이디)
     name: str # 이름
     nickname: str # 닉네임
     phon_num: str # 전화번호
+    class Config: 
+        orm_mode = True
 
 class UpdateModel(BaseModel): # 마이페이지 수정 모델 (선택적)
     email: str | None = None # 이메일(아이디)
     nickname: str | None = None # 닉네임
     phon_num: str | None = None # 전화번호
+    class Config: # 
+        orm_mode = True
 
 class UpdatePwd(BaseModel): # 비밀번호 변경 모델
     email : str # 이메일(아이디) 
     new_pwd : str | None = None # 새 비밀번호
     pwd : str | None = None # 비밀번호 
+    class Config: # 
+        orm_mode = True
 
 class MemberCreate(BaseModel): # 회원가입
     email: str
@@ -42,13 +48,19 @@ class MemberCreate(BaseModel): # 회원가입
     nickname: str
     pwd: str
     phon_num: str
+    class Config:
+        orm_mode = True
 
 class FindID(BaseModel): # 아이디 찾기 모델
     name: str # 이름
     phon_num: str # 전화번호
+    class Config: 
+        orm_mode = True
     
 class FindPwd(BaseModel): # 비밀번호 찾기 모델
     email: str # 이메일(아이디)
+    class Config: 
+        orm_mode = True
 
 
 class Member(MemberBase): # member 테이블의 전체 스키마(MemberBase) 데이터 지정
